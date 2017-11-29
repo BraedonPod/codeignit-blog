d593858106562a682d84cbc75737c0c1dc86d731
BraedonPod <braedonpod@gmail.com>
2017-11-29 16:08:13 +0000

    Readme

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,7 +2,7 @@
 	<head>
 		<title>Silent Words</title>
 		<link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.js"></script>
     <script src="https://cdn.ckeditor.com/ckeditor5/1.0.0-alpha.1/classic/ckeditor.js"></script>
@@ -11,22 +11,19 @@
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
         <div class="navbar-header">
-          <a class="navbar-brand" href="<?php echo base_url(); ?>">Silent Words</a>
+          <a class="navbar-brand" href="/">Silent Words</a>
         </div>
         <div id="navbar">
           <ul class="nav navbar-nav">
-             <li><a href="<?php echo base_url(); ?>posts">Posts</a></li>
-             <li><a href="<?php echo base_url(); ?>categories">Categories</a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-          <?php if(!$this->session->userdata('logged_in')) : ?>
-            <li><a href="<?php echo base_url(); ?>users/login">Login</a></li>
-            <li><a href="<?php echo base_url(); ?>users/register">Register</a></li>
+             <li class="nav-item"><a class="nav-link" href="/posts">Posts</a></li>
+             <li class="nav-item"><a class="nav-link" href="/categories">Categories</a></li>
+           <?php if(!$this->session->userdata('logged_in')) : ?>
+            <li class="nav-item"><a class="nav-link" href="/users/login">Login</a></li>
+            <li class="nav-item"><a class="nav-link" href="/users/register">Register</a></li>
           <?php endif; ?>
           <?php if($this->session->userdata('logged_in')) : ?>
-            <li><a href="<?php echo base_url(); ?>posts/create">Create Post</a></li>
-            <!--<li><a href="<?php echo base_url(); ?>categories/create">Create Category</a></li>-->
-            <li><a href="<?php echo base_url(); ?>users/logout">Logout</a></li>
+            <li class="nav-item"><a class="nav-link" href="/posts/create">Create Post</a></li>
+            <li class="nav-item"><a class="nav-link" href="/users/logout">Logout</a></li>
           <?php endif; ?>
           </ul>
         </div>
